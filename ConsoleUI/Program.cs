3cb07e0e0b26e9cc3c77de86f8a09bd1fa3795d3
Faruk Tekbaş <faruktekbas60@gmail.com>
2021-12-29 13:51:17 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+
+
+using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+
+CarManager carManager = new CarManager(new InMemoryCarDal());
+
+foreach (var item in carManager.GetAll())
+{
+    Console.WriteLine(item.Description);
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,57 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car> 
+            {
+                new Car{Id = 1,BrandId=1,ColorId=1,ModelYear=2010,DailyPrice=135000,Description="Beyaz Sedan BMW M4 Hatasız"},
+                new Car{Id = 2,BrandId=2,ColorId=1,ModelYear=2015,DailyPrice=100000,Description="Beyaz Sedan Renault Clio Hatasız"},
+                new Car{Id = 3,BrandId=3,ColorId=2,ModelYear=2021,DailyPrice=185000,Description="Mavi Station Mercedes C180 Kazalı"},
+                new Car{Id = 4,BrandId=2,ColorId=3,ModelYear=2020,DailyPrice=210000,Description="Siyah Sedan Renault Captur Hatasız"},
+                new Car{Id = 5,BrandId=5,ColorId=4,ModelYear=2018,DailyPrice=195000,Description="Beyaz Sedan Ford Focus Hasarlı"},
+                new Car{Id = 6,BrandId=3,ColorId=3,ModelYear=2010,DailyPrice=200000,Description="Siyah Station Mercedes E220 Hatasız"},
+            };
+        }
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete= _cars.SingleOrDefault(c=>c.Id==car.Id);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public int GetById(Car car)
+        {
+            return Convert.ToInt32(_cars.Where(c=>c.Id== car.Id));
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+           carToUpdate.BrandId = car.BrandId;
+           carToUpdate.ColorId = car.ColorId;
+           carToUpdate.ModelYear = car.ModelYear;
+           carToUpdate.DailyPrice = car.DailyPrice;
+           carToUpdate.Description = car.Description;
+        }
+    }
+}
