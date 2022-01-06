09ff69729d6f69fc55072fc4e680f2ac3336b064
Faruk Tekbaş <faruktekbas60@gmail.com>
2022-01-06 12:24:10 +0300

    12.gun odevli

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,5 +1,7 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspect.Autofac.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -21,6 +23,7 @@ namespace Business.Concrete
             _carDal = carDal;
         }
 
+        [ValidationAspect(typeof(CarValidator))]
         public IResult Add(Car car)
         {
             if (car.Name.Length < 2)
---
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -0,0 +1,28 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CarValidator : AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(p => p.Name).NotEmpty();
+            RuleFor(p => p.Name).MinimumLength(2);
+            RuleFor(p => p.DailyPrice).NotEmpty();
+            RuleFor(p => p.DailyPrice).GreaterThan(0);
+            RuleFor(p => p.ModelYear).GreaterThanOrEqualTo(2010).When(p => p.BrandId == 1);
+            RuleFor(p => p.Name).Must(StartWithA).WithMessage("Ürünler A harfi ile başlamalı.");
+        }
+
+        private bool StartWithA(string arg)
+        {
+            return arg.StartsWith("A");
+        }
+    }
+}
